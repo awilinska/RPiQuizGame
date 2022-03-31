a3566188314611eaf8495546b14b645ab8a381b3
Alicja Wilińska <72753653+awilinska@users.noreply.github.com>
2022-03-31 18:37:28 +0200

    Update text_creator.py

--- text_creator.py
+++ text_creator.py
@@ -1,5 +1,5 @@
-
 import pygame
+
 black = (0, 0, 0)
 gray = (127, 127, 127)
 white = (255, 255, 255)
@@ -16,12 +16,12 @@ height = 700
 display = pygame.display.set_mode((width, height))
 
 def textdraw(text, text_height, color, font_size):
-    font = pygame.font.SysFont('arial', font_size)
+    font = pygame.font.Font('/home/pi/Desktop/Play-Regular.ttf', font_size)
     text = font.render(text, True, color)
     text_rect = text.get_rect(center=(width//2, text_height))
     display.blit(text, text_rect)
 
 def customtextdraw(text, position, color, font_size):
-    font = pygame.font.SysFont('arial', font_size)
+    font = pygame.font.SysFont('/home/pi/Desktop/Play-Regular.ttf', font_size)
     text = font.render(text, True, color)
     display.blit(text, position)
