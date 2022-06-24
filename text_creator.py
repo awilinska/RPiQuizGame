7f5af844ad8605203e93cb4d2a5ff9b35472ed2c
Alicja Wilińska <72753653+awilinska@users.noreply.github.com>
2022-06-24 16:32:01 +0200

    Update text_creator.py

--- text_creator.py
+++ text_creator.py
@@ -1,17 +1,5 @@
 import pygame
-
-black = (0, 0, 0)
-gray = (127, 127, 127)
-white = (255, 255, 255)
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-yellow = (255, 255, 0)
-cyan = (0, 255, 255)
-magenta = (255, 0, 255)
-
-width = 1100
-height = 700
+from variables import *
 
 display = pygame.display.set_mode((width, height))
 
