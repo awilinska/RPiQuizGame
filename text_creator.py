dfb37aee576caf5a5b7e1b91309be63566fb16df
Alicja Wilińska <72753653+awilinska@users.noreply.github.com>
2022-07-01 21:43:48 +0200

    Update text_creator.py

--- text_creator.py
+++ text_creator.py
@@ -4,12 +4,12 @@ from variables import *
 display = pygame.display.set_mode((width, height))
 
 def textdraw(text, text_height, color, font_size):
-    font = pygame.font.Font('/home/pi/Desktop/Play-Regular.ttf', font_size)
+    font = pygame.font.Font('/home/pi/Desktop/RPI Quiz Game/Play-Regular.ttf', font_size)
     text = font.render(text, True, color)
     text_rect = text.get_rect(center=(width//2, text_height))
     display.blit(text, text_rect)
 
 def customtextdraw(text, position, color, font_size):
-    font = pygame.font.SysFont('/home/pi/Desktop/Play-Regular.ttf', font_size)
+    font = pygame.font.Font('/home/pi/Desktop/RPI Quiz Game/Play-Regular.ttf', font_size)
     text = font.render(text, True, color)
     display.blit(text, position)
